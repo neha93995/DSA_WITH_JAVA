cc8af0d99199f1c2da8e422b9d450fb721e2fdb2
neha93995 <nehabaghelcs190302@gmail.com>
2024-01-20 21:23:33 +0530

    day 7

---
+++ Q10_Minimum_Number_Of_Jumps.java
@@ -0,0 +1,92 @@
+public class Q10_Minimum_Number_Of_Jumps {
+
+    // Time Complexity O(n^2)
+    // space complexity O(1)
+    static int minJumps(int[] arr) {
+
+        if (arr[0] == 0) {
+            return -1;
+        }
+
+        int steps = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int index = i;
+            int max = arr[i];
+
+            if (arr[i] + i > arr.length) {
+                steps++;
+                break;
+            }
+            for (int j = 1; j <= arr[i]; j++) {
+
+                if (arr[i + j] > max) {
+                    max = arr[i + j];
+                    index = i + j;
+                }
+            }
+            System.out.println(i + " -> " + index);
+            i = index - 1;
+            steps++;
+
+        }
+
+        return steps;
+
+    }
+
+    static int minJumps2(int[] arr) {
+        if (arr.length <= 1)
+            return 0;
+
+        if (arr[0] >= arr.length - 1)
+            return 1;
+
+        if (arr[0] == 0)
+            return -1;
+
+        int maxReach = arr[0];
+        int step = arr[0];
+        int jump = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            System.out.println("i: "+i+" maxReach: "+maxReach+" step: "+step+" jump: "+jump);
+            if (i == arr.length - 1)
+                return jump;
+
+            if (arr[i] >= (arr.length - 1) - i)
+                return jump + 1;
+
+            maxReach = Math.max(maxReach, i + arr[i]);
+
+            step--;
+
+            if (step == 0) {
+
+                jump++;
+
+                if (i >= maxReach)
+                    return -1;
+
+                step = maxReach - i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    
+
+    public static void main(String[] args) {
+
+        // int arr[] = { 1, 4, 3, 2, 6, 7 };?
+        int arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} ;
+
+        System.out.println("steps " + minJumps(arr));
+        System.out.println("steps " + minJumps2(arr));
+
+    }
+}
---
+++ Q11_Find_Duplicate_in_Array.java
@@ -0,0 +1,107 @@
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+
+
+public class Q11_Find_Duplicate_in_Array{
+
+    //Timpe Complexity = O(n)
+    // Space Complexity = O(n)
+
+    public static int findDuplicate1(int arr[])
+    {
+        int a[] = new int[arr.length+1];
+
+
+        for(int i=0; i<arr.length; i++)
+        {
+            int curr = arr[i];
+            if(a[curr]>0)
+            {
+                return curr;
+            }
+            else{
+                a[curr]  = a[curr]+1;
+            }
+        }
+        return -1;
+    }
+
+    // Time Complexity = O(n)
+    // Space Complexity =O(1)
+    // With changing the original array
+
+    public static int findDuplicate2(int arr[])
+    {
+        
+        while(true)
+        {   
+            int temp = arr[0];
+            arr[0] = arr[temp];
+            arr[temp] = temp;
+            if(arr[0]==arr[temp])
+            {
+                return arr[0];
+            }
+        }
+        
+    }
+
+    // Time Complexity = O(n)
+    // Space Complexity =O(1)
+    // With changing the original array
+
+    public static int findDuplicate3(int arr[])
+    {
+
+        for(int i=0; i<arr.length; i++)
+        {
+            int curr = Math.abs(arr[i]);
+            if(arr[curr]<0)
+            {
+                return curr;
+            }
+            arr[curr] = arr[curr]*-1;
+
+        }
+
+        return -1;
+    }
+
+
+    // Time Complexity = O(n)
+    // Space Complexity =O(1)
+    // Without changing the original array
+
+    public static int findDuplicate4(int arr[])
+    {
+
+        int duplicate = -1;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            int curr = Math.abs(arr[i]);
+            if(arr[curr]<0)
+            {
+                duplicate = curr;
+                break;
+            }
+            arr[curr] = arr[curr]*-1;
+
+        }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i]  = Math.abs(arr[i]);
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,4,5,1};
+
+        
+        System.out.println(findDuplicate3(arr));
+    }
+}
